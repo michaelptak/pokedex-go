b448c850dbc9d223314751cbfe148eac4aa7a39f
Michael Ptak <michael.p11@proton.me>
2026-04-02 13:22:06 -0400

    started cache

---
+++ internal/pokecache/cache.go
@@ -0,0 +1,42 @@
+package pokecache
+
+import (
+	"sync"
+	"time"
+)
+
+type Cache struct {
+	cacheMap map[string]cacheEntry
+	mu       sync.Mutex
+	interval time.Duration
+}
+
+type cacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
+
+func NewCache(interval time.Duration) *Cache {
+	return &Cache{
+		interval: interval,
+	}
+}
+
+func (c *Cache) Add(key string, val []byte) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.cacheMap[key] = cacheEntry{
+		val:       val,
+		createdAt: time.Now(),
+	}
+}
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	entry, exists := c.cacheMap[key]
+	if !exists {
+		return nil, false
+	}
+	return entry.val, true
+}
